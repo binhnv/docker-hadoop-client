c3a326ba0de4567b3dc6dbabae7e74ac5b4367aa
Binh Van Nguyen <binhnv80@gmail.com>
2017-05-01 11:57:01 -0700

    Add Unzip, Avro, Boto, Jinja2 and JayDeBeApi packages

--- scripts/build/install.sh
+++ scripts/build/install.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-g_pyhocon_ver="0.3.30"
+g_pyhocon_ver="0.3.35"
 g_mysql_python_ver="1.2.5"
 g_kafka_python_ver="1.2.3"
 g_vertica_python_ver="0.6.5"
 g_iso8601_version="0.1.11"
-g_click_version="6.6"
+g_click_version="6.7"
 g_click_log_version="0.1.4"
 g_impyla_version="0.13.8"
 g_snakebite_version="2.8.2"
@@ -17,6 +17,10 @@ g_sqlalchemy_version="1.0.14"
 g_setuptools_version="24.0.2"
 g_thrift_sasl_version="0.2.1"
 g_thrift_version="0.9.3"
+g_avro_version="1.8.1"
+g_boto3_version="1.4.4"
+g_jinja2_version="2.9.5"
+g_jaydebeapi_version="1.1.1"
 
 g_spark_home=${SPARK_HOME}
 g_sqoop_home=${SQOOP_HOME}
@@ -29,7 +33,7 @@ g_flink_home=${FLINK_HOME}
 g_flink_bin_url=${FLINK_BIN_URL}
 
 function install_spark {
-    echo "Downloading Spark from ${g_spark_bin_url}..."
+    echo "Downloading ${g_spark_bin_url}..."
     mkdir -p ${g_spark_home}
     curl -sL ${g_spark_bin_url} | tar -xz -C ${g_spark_home}
     ln -s ${g_hive_conf_dir}/hive-site.xml ${g_spark_conf_dir}/hive-site.xml
@@ -39,7 +43,7 @@ function install_spark {
 }
 
 function install_sqoop {
-    echo "Downloading Sqoop from ${g_sqoop_bin_url}..."
+    echo "Downloading ${g_sqoop_bin_url}..."
     mkdir -p ${g_sqoop_home}
     curl -sL ${g_sqoop_bin_url} | tar -xz -C ${g_sqoop_home} --strip-component=1
     ln -s ${g_mysql_connector_jar} ${g_sqoop_home}/lib/mysql-connector-java.jar
@@ -65,7 +69,8 @@ function install_os_packages {
         build-essential \
         libkrb5-dev \
         cyrus-sasl2-mit-dbg \
-        libsasl2-modules-gssapi-mit
+        libsasl2-modules-gssapi-mit \
+        unzip
 }
 
 function install_python_packages {
@@ -85,7 +90,11 @@ function install_python_packages {
         iso8601==${g_iso8601_version} \
         click==${g_click_version} \
         click_log==${g_click_log_version} \
-        thrift_sasl==${g_thrift_sasl_version}
+        thrift_sasl==${g_thrift_sasl_version} \
+        avro==${g_avro_version} \
+        boto3==${g_boto3_version} \
+        jinja2==${g_jinja2_version} \
+        jaydebeapi==${g_jaydebeapi_version}
 }
 
 function cleanup {
